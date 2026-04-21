2b94ff9f28c2a910edc1c88d06c36d5bd7365615
ArthurKauan <arthurkaun00@gmail.com>
2026-04-21 09:54:25 -0300

    Atualizando...

---
+++ desafios e exercicios/ex021.py
@@ -0,0 +1,12 @@
+import pygame
+
+pygame.init()
+pygame.mixer.music.load('desafios e exercicios/ex021.mp3')
+pygame.mixer.music.play()
+input('Dê Enter para parar a música...')
+#import time
+#time.sleep(5)
+#pygame.mixer.music.stop()
+
+#while pygame.mixer.music.get_busy(): 
+#    pass
